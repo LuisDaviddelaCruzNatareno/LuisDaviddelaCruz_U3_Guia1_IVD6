f85d0f9530510403ee0fbae362a758a336fcbbb7
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-06-17 22:02:28 -0600

    Add files via upload

---
+++ C#/Act3_Inciso1_Lecc16.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool repetido = false;
+
+        int[] numeros = new int[16];
+
+        for (int i = 1; i <= 15; i++)
+        {
+            Console.Write("Escribe un número: ");
+
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 1; i <= 14; i++)
+        {
+            for (int j = i + 1; j <= 15; j++)
+            {
+                if (numeros[i] == numeros[j])
+                {
+                    repetido = true;
+                }
+            }
+        }
+
+        if (repetido)
+        {
+            Console.WriteLine("Existe al menos un número repetido");
+        }
+        else
+        {
+            Console.WriteLine("No existen números repetidos");
+        }
+
+        Console.ReadKey();
+    }
+}
