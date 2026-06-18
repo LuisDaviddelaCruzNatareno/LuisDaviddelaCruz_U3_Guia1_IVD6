a6fb0a5f166df7b41269a300c4f07c81316d7657
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-06-17 21:39:08 -0600

    Add files via upload

---
+++ C#/Guia13uLdelaCruz.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numeroColor;
+
+        string[] colores = new string[9];
+
+        colores[0] = "cafe";
+        colores[1] = "cafe claro";
+        colores[2] = "anaranjado";
+        colores[3] = "rojo";
+        colores[4] = "amarillo";
+        colores[5] = "negro";
+        colores[6] = "morado";
+        colores[7] = "morado oscuro";
+
+        Console.Write("Escribe el número del color de las temperaz: ");
+        numeroColor = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("El color es: " + colores[numeroColor]);
+
+        Console.ReadKey();
+    }
+}
