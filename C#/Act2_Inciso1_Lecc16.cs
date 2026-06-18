47329a0bf92b66b4e92936683b9a8b79e086dfdf
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-06-17 21:49:26 -0600

    Add files via upload

---
+++ C#/Act2_Inciso1_Lecc16.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int contador = 0;
+
+        int[] edades = new int[8];
+
+        for (int i = 1; i <= 7; i++)
+        {
+            Console.Write("Escribe la edad de la persona " + i + ": ");
+
+            edades[i] = int.Parse(Console.ReadLine());
+
+            if (edades[i] >= 18)
+            {
+                contador++;
+            }
+        }
+
+        Console.WriteLine("La cantidad de personas mayores de edad es: " + contador);
+
+        Console.ReadKey();
+    }
+}
